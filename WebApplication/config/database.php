4498dc2809945941852c43997a6f33ae0c54bcf6
Alex Taylor <amt22@aber.ac.uk>
2017-02-25 14:05:42 +0000

    set up travis to work with quiz system

--- WebApplication/config/database.php
+++ WebApplication/config/database.php
@@ -66,6 +66,18 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'quiz_test'),
+            'username'  => env('DB_TEST_USERNAME', 'quiz_test'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
     ],
 
     /*
