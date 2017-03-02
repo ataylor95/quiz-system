b55bcd0dbf2842b250c642cb0c06da1d5fe77066
Alex Taylor <amt22@aber.ac.uk>
2017-03-02 12:33:57 +0000

    added simple test for login

---
+++ WebApplication/tests/Feature/AuthTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testNeedToLoginToBackend()
+    {
+        $response = $this->get('home');
+        $response->assertRedirect('login');
+    }
+}
