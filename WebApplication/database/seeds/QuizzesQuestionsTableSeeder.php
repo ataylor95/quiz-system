207564fec2ebf6b57b3ea80f5246045a1d140f64
Alex Taylor <amt22@aber.ac.uk>
2017-03-06 12:05:33 +0000

    quizzesquestions seed creates some quizzes, questions and users, also adds extra questions to user of id 1 quizzes

--- WebApplication/database/seeds/QuizzesQuestionsTableSeeder.php
+++ WebApplication/database/seeds/QuizzesQuestionsTableSeeder.php
@@ -6,11 +6,22 @@ class QuizzesQuestionsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
+     * This created three quizzes for the first user
+     * It then also adds an extra question to each of these
+     * Then it creates a further 10 users, quizzes and questions
      * @return void
      */
     public function run()
     {
-        //
+        factory('App\Quiz', 3)->create(['user_id' => 1]);
+        factory('App\Question', 3)->create();
+        for ($i=1;$i<=3;$i++) {
+            factory('App\QuizQuestion')->create([
+                'quiz_id' => $i, 
+                'question_id' => $i
+            ]);
+            factory('App\QuizQuestion')->create(['quiz_id' => $i]);
+        }
+        factory('App\QuizQuestion', 10)->create();
     }
 }
