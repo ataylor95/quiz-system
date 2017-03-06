69c932b1add230afa7eb223e8121a3bbfb18f38e
Alex Taylor <amt22@aber.ac.uk>
2017-03-06 10:51:43 +0000

    removing clutter from quiz seeder

--- WebApplication/database/seeds/QuizzesTableSeeder.php
+++ WebApplication/database/seeds/QuizzesTableSeeder.php
@@ -12,19 +12,6 @@ class QuizzesTableSeeder extends Seeder
      */
     public function run()
     {
-        /*$quiz1 = new Quiz;
-        $quiz2 = new Quiz;
-        $quiz3 = new Quiz;
-        $quizzes = [$quiz1, $quiz2, $quiz3];
-
-        $quiz1->user_id = 1;
-        $quiz2->user_id = 1;
-        $quiz3->user_id = 2;
-
-        foreach ($quizzes as $quiz) {
-            $quiz->name = str_random(5);
-            $quiz->save();
-        }*/
         factory('App\Quiz', 3)->create(['user_id' => 1]);
         factory('App\Quiz', 17)->create();
     }
