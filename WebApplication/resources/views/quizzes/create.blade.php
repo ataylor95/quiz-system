41d3bde366acc3914e14c3e29d42ef8477169d94
Alex Taylor <amt22@aber.ac.uk>
2017-03-06 17:32:19 +0000

    created form to create a new quiz

--- WebApplication/resources/views/quizzes/create.blade.php
+++ WebApplication/resources/views/quizzes/create.blade.php
@@ -5,20 +5,21 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <form action=''>
-                    <div class='form-group'>
-                        <label for='name'>Name of Quiz:</label>
-                        <input type='text' class='form-control' id='name' name='name'>
+                <form method="POST" action="/quizzes">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name">Name of Quiz:</label>
+                        <input type="text" class="form-control" id="name" name="name">
                     </div>
 
-                    <div class='form-group'>
-                        <label for='desc'>Description:</label>
-                        <textarea type='text' class='form-control' id='desc' name='desc'></textarea>
+                    <div class="form-group">
+                        <label for="desc">Description:</label>
+                        <textarea type="text" class="form-control" id="desc" name="desc"></textarea>
                     </div>
                     
                     <p>Questions are added in the next step</p>
 
-                    <button type='submit' class='btn center-block btn-default'>Create</button> 
+                    <button type="submit" class="btn center-block btn-default">Create</button> 
                 </form>
             </div>
         </div>
