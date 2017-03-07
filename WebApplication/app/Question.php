87b6facb2baa7c4524caaf73387cef9097147dc0
Alex Taylor <amt22@aber.ac.uk>
2017-03-07 15:56:03 +0000

    setting the fillable fields for quizzes and questions

--- WebApplication/app/Question.php
+++ WebApplication/app/Question.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = [
+        'question_text', 'type', 'answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'answer6', 'answer7', 'answer8', 'answer9', 'answer10', 
+    ];
 }
--- WebApplication/app/QuizQuestion.php
+++ WebApplication/app/QuizQuestion.php
@@ -8,4 +8,7 @@ class QuizQuestion extends Model
 {
     //
     protected $table = 'quizzes_questions';
+    protected $fillable = [
+        'quiz_id', 'question_id'
+    ];
 }
