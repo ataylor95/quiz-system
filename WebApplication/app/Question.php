a54138e3a4fc48116e2518af48eaff01c7a3c32c
Alex Taylor <amt22@aber.ac.uk>
2017-03-12 15:37:54 +0000

    adding function to get quiz that the question belongs to

--- WebApplication/app/Question.php
+++ WebApplication/app/Question.php
@@ -11,6 +11,11 @@ class Question extends Model
         'question_text', 'type', 'answer1', 'answer2', 'answer3', 'answer4', 'answer5', 'answer6', 'answer7', 'answer8', 'answer9', 'answer10', 
     ];
 
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+
     /**
      * Saves a question to the table
      *
