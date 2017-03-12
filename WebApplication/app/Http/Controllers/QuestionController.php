4120b4c57dfb045de66dda6a55739777899bf11d
Alex Taylor <amt22@aber.ac.uk>
2017-03-12 16:06:43 +0000

    removing the quizquestion model and all references to it

--- WebApplication/app/Http/Controllers/QuestionController.php
+++ WebApplication/app/Http/Controllers/QuestionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Question;
-use App\QuizQuestion;
 
 class QuestionController extends Controller
 {
--- WebApplication/app/Quiz.php
+++ WebApplication/app/Quiz.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Question;
-use App\QuizQuestion;
 
 class Quiz extends Model
 {
