a09c246c18c0fc9ebb6151f57b446ae9f12c63c4
Alex Taylor <amt22@aber.ac.uk>
2017-03-15 16:47:41 +0000

    adding validation tests for creating new quizzes

--- WebApplication/tests/Browser/CreateNewQuizTest.php
+++ WebApplication/tests/Browser/CreateNewQuizTest.php
@@ -4,11 +4,13 @@ namespace Tests\Browser;
 
 use Tests\DuskTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
 
 class CreateNewQuizTest extends DuskTestCase
 {
     use DatabaseMigrations;
+
     /**
      * A Dusk test example.
      *
@@ -33,6 +35,61 @@ class CreateNewQuizTest extends DuskTestCase
                     ->assertSee($quizName)
                     ->assertSee($quizDesc)
                     ->assertSee('Add Question');
+        });
+    }
+
+    /**
+     * Test the frontend validation when creating new quiz
+     * This test is not fantastic, but it kind of works
+     *
+     * @return void
+     */
+    public function testHTMLValidationQuiz()
+    {
+        $quizName = "";
+        $quizDesc = "";
+
+        $user = factory(User::class)->create(['id' => 2]);
+
+        /*So we check to see that the browser keeps you on the same page
+          This is because html validation is hard to see, also it might
+          be different on different drivers like PhantomJS vs Chrome*/
+        $this->browse(function ($browser) use ($user, $quizName, $quizDesc) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->clickLink('New Quiz')
+                    ->type('name', $quizName)
+                    ->type('desc', $quizDesc)
+                    ->press('Create')
+                    ->assertPathIs('/quizzes/create');
+        });
+    }
+
+    /**
+     * Test the backend validation when creating new quiz
+     * Name should be minimum 5 characters
+     *
+     * @return void
+     */
+    public function testValidationQuiz()
+    {
+        $quizName = "Test"; //Lets try a 4 letter name to fail
+        $quizDesc = "Test"; //None on the length of the description
+
+        $user = factory(User::class)->create(['id' => 3]);
+
+        $this->browse(function ($browser) use ($user, $quizName, $quizDesc) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->clickLink('New Quiz')
+                    ->type('name', $quizName)
+                    ->type('desc', $quizDesc)
+                    ->press('Create')
+                    ->assertSee('The name must be at least 5 characters');
         });
     }
 }
