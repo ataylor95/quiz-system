7556d31e51c1c355048d5676232bb827905fb154
Alex Taylor <amt22@aber.ac.uk>
2017-03-15 19:29:48 +0000

    more testing of the quiz functionality

--- WebApplication/tests/Browser/CreateNewQuizTest.php
+++ WebApplication/tests/Browser/CreateNewQuizTest.php
@@ -6,6 +6,8 @@ use Tests\DuskTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
+use App\Quiz;
+use App\Question;
 
 class CreateNewQuizTest extends DuskTestCase
 {
@@ -90,6 +92,54 @@ class CreateNewQuizTest extends DuskTestCase
                     ->type('desc', $quizDesc)
                     ->press('Create')
                     ->assertSee('The name must be at least 5 characters');
+        });
+    }
+
+    /**
+     * Test whether a quiz owns a question
+     *
+     * @return null
+     */
+    public function testQuizContainsQuestions()
+    {
+        $user = factory(User::class)->create(['id' => 4]);
+        $quiz = factory(Quiz::class)->create(['user_id' => 4]);
+        $question = factory(Question::class)->create(['quiz_id' => $quiz->id]);
+
+        $this->browse(function ($browser) use ($user, $question) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->clickLink('View') //Only one quiz so it will be the only 'View' link
+                    ->assertSee($question->question_text);
+        });
+    }
+
+    /**
+     * Test adding a question to a quiz
+     *
+     * @return null
+     */
+    public function testAddQuestion()
+    {
+        $user = factory(User::class)->create(['id' => 5]);
+        $quiz = factory(Quiz::class)->create(['user_id' => 5]);
+
+        $this->browse(function ($browser) use ($user, $quiz) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->clickLink('View') //Only one quiz so it will be the only 'View' link
+                    ->clickLink('Add Question')
+                    ->type('question_text', 'Test Question')
+                    ->select('type', 'multi_select')
+                    ->type('answer1', 'Hello')
+                    ->type('answer2', 'Boop')
+                    ->press('Create')
+                    ->assertSee($quiz->name)
+                    ->assertSee('Test Question');
         });
     }
 }
