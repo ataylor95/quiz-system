ba59b9ec761ecde9643d0740430ac74fc9b90825
Alex Taylor <amt22@aber.ac.uk>
2017-03-17 11:24:08 +0000

    edit tests factories are a little more sensible

--- WebApplication/tests/Browser/EditExistingQuizTest.php
+++ WebApplication/tests/Browser/EditExistingQuizTest.php
@@ -20,7 +20,7 @@ class EditExistingQuizTest extends DuskTestCase
     public function testDeleteQuiz()
     {
         $user = factory(User::class)->create();
-        $quiz = factory(Quiz::class)->create(['user_id' => 1]);
+        $quiz = factory(Quiz::class)->create(['user_id' => $user->id]);
 
         $this->browse(function ($browser) use ($user, $quiz) {
             $browser->loginAs($user->id)
@@ -40,7 +40,7 @@ class EditExistingQuizTest extends DuskTestCase
     public function testDeleteQuestion()
     {
         $user = factory(User::class)->create(['id' => 2]);
-        $quiz = factory(Quiz::class)->create(['user_id' => 2]);
+        $quiz = factory(Quiz::class)->create(['user_id' => $user->id]);
         $question = factory(Question::class)->create(['quiz_id' => $quiz->id]);
 
         $this->browse(function ($browser) use ($user, $quiz, $question) {
