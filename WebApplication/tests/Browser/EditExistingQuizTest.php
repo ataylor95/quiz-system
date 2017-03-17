553c7459c2e758dc704e3c252f14be11d451a466
Alex Taylor <amt22@aber.ac.uk>
2017-03-17 11:57:42 +0000

    added tests for the editing quiz pages

--- WebApplication/tests/Browser/EditExistingQuizTest.php
+++ WebApplication/tests/Browser/EditExistingQuizTest.php
@@ -61,6 +61,22 @@ class EditExistingQuizTest extends DuskTestCase
      */
     public function testEditQuiz()
     {
+        $user = factory(User::class)->create(['id' => 3]);
+        $quiz = factory(Quiz::class)->create(['user_id' => $user->id]);
+        $message = "42 is the answer to life, the universe and everything";
+
+        $this->browse(function ($browser) use ($user, $quiz, $message) {
+            $browser->loginAs($user->id)
+                    ->visit('/quizzes')
+                    ->clickLink('View')
+                    ->clickLink('Edit')
+                    ->assertInputValue('name', $quiz->name)
+                    ->assertInputValue('desc', $quiz->desc)
+                    ->type('name', $message)
+                    ->press('Update')
+                    ->assertPathIs('/quizzes/' . $quiz->id) //Make sure directed to quiz
+                    ->assertSee($message);
+        });
     }
 
     /**
@@ -70,5 +86,22 @@ class EditExistingQuizTest extends DuskTestCase
      */
     public function testEditQuestion()
     {
+        $user = factory(User::class)->create(['id' => 4]);
+        $quiz = factory(Quiz::class)->create(['user_id' => $user->id]);
+        $question = factory(Question::class)->create(['quiz_id' => $quiz->id]);
+        $message = "Its turtles all the way down";
+
+        $this->browse(function ($browser) use ($user, $quiz, $question, $message) {
+            $browser->loginAs($user->id)
+                    ->visit('/questions/' . $question->id . '/edit?quiz=' . $quiz->id)
+                    ->assertInputValue('question_text', $question->question_text)
+                    ->assertSelected('type', $question->type)
+                    ->assertInputValue('answer1', $question->answer1)
+                    ->assertInputValue('answer2', $question->answer2)
+                    ->type('question_text', $message)
+                    ->press('Update')
+                    ->assertPathIs('/quizzes/' . $quiz->id) //Make sure directed to quiz
+                    ->assertSee($message);
+        });
     }
 }
