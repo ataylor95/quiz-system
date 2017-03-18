53e3774910a8008823f2e4a6e69c74333c85a591
Alex Taylor <amt22@aber.ac.uk>
2017-03-18 20:54:27 +0000

    adding echo library for using the pusher stuff on the front end

--- WebApplication/resources/assets/js/bootstrap.js
+++ WebApplication/resources/assets/js/bootstrap.js
@@ -38,9 +38,9 @@ window.axios.defaults.headers.common = {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from "laravel-echo"
+import Echo from "laravel-echo"
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '3f21a6176f0f0d31c04c'
+});
