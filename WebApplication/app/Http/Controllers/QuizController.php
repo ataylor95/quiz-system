d9d9311e2fd8e080c0cc464d8ea020215c561566
Alex Taylor <amt22@aber.ac.uk>
2017-03-21 16:41:38 +0000

    we dont want auth check on the quiz action

--- WebApplication/app/Http/Controllers/QuizController.php
+++ WebApplication/app/Http/Controllers/QuizController.php
@@ -15,7 +15,7 @@ class QuizController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['quiz']]);
     }
 
     /**
