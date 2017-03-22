67291c8d4c35258a2fae87939fb76df4f808b163
Alex Taylor <amt22@aber.ac.uk>
2017-03-22 15:44:31 +0000

    adding some basic buttons to the admin panel, and a container

--- WebApplication/resources/views/quizzes/admin-panel.blade.php
+++ WebApplication/resources/views/quizzes/admin-panel.blade.php
@@ -1,4 +1,16 @@
 @if (Auth::check()) 
     {{-- TODO: right now any auth user can see this, only want the owner--}}
-    <p>Testing admin perms</p>
+    <div class="admin-panel">
+        <div class='row'>
+            <div class="col-xs-1">
+                <button class="btn btn-primary">Prev</button>
+            </div>
+            <div class="col-xs-1">
+                <button class="btn btn-primary">Next</button>
+            </div>
+            <div class="col-xs-2">
+                <button class="btn btn-primary">End Quiz</button>
+            </div>
+        </div>
+    </div>
 @endif
