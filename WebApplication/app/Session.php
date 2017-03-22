4548b6d1d092b11fc1b2be8e79404c7279797afc
Alex Taylor <amt22@aber.ac.uk>
2017-03-22 17:35:30 +0000

    function to get the session key via user id

--- WebApplication/app/Session.php
+++ WebApplication/app/Session.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Session extends Model
 {
     //
+
+    /**
+     * Gets the session key used in the channel name
+     *
+     * @param int $userID
+     * @return Collection
+     */
+    public static function getSessionKey($userID)
+    {
+       return Session::where('user_id', $userID)->get(['session_key']); 
+    } 
 }
