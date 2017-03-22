f25e706e9d75a5468a87d30df881b7061bc9c6b3
Alex Taylor <amt22@aber.ac.uk>
2017-03-22 16:06:40 +0000

    adding id and name  to the buttons of admin panel

--- WebApplication/resources/views/quizzes/admin-panel.blade.php
+++ WebApplication/resources/views/quizzes/admin-panel.blade.php
@@ -3,13 +3,13 @@
     <div class="admin-panel">
         <div class='row'>
             <div class="col-xs-1">
-                <button class="btn btn-primary">Prev</button>
+                <button id="prev-quiz" name="prev-quiz" class="btn btn-primary">Prev</button>
             </div>
             <div class="col-xs-1">
-                <button class="btn btn-primary">Next</button>
+                <button id="next-quiz" name="next-quiz" class="btn btn-primary">Next</button>
             </div>
             <div class="col-xs-2">
-                <button class="btn btn-primary">End Quiz</button>
+                <button id="end-quiz" name="end-quiz" class="btn btn-danger">End Quiz</button>
             </div>
         </div>
     </div>
