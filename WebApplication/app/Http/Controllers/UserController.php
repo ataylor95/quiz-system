faf888ff5b660f22e262d5b314cd760a0c403dc2
Alex Taylor <amt22@aber.ac.uk>
2017-03-27 17:38:28 +0100

    action for rendering the users.edit page created

--- WebApplication/app/Http/Controllers/UserController.php
+++ WebApplication/app/Http/Controllers/UserController.php
@@ -68,9 +68,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        $sessionKey = Session::where('user_id', '=', $user->id)->get()[0]->session_key;
+        return view('users.edit', compact('user', 'sessionKey')); 
     }
 
     /**
