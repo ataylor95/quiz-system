b2ec44f75f6d78f299aa0f0393a8676826cbe45f
Alex Taylor <amt22@aber.ac.uk>
2017-03-27 11:56:12 +0100

    making sure to use the route function for routing rather than hardcoding

--- WebApplication/resources/views/questions/create.blade.php
+++ WebApplication/resources/views/questions/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <form method="POST" action="/questions">
+                <form method="POST" action="{{route('questions.store')}}">
                     <input name="quiz_id" type="hidden" value="{{ app('request')->input('quiz') }}">
                     {{ csrf_field() }}
                     <div class="form-group">
--- WebApplication/resources/views/questions/edit.blade.php
+++ WebApplication/resources/views/questions/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <form method="POST" action="/questions/{{$question['id']}}">
+                <form method="POST" action="{{route('questions.update', ['id' => $question['id']])}}">
                     {{ csrf_field() }}
                     {{ method_field('PATCH') }}
                     <input name="quiz_id" type="hidden" value="{{ app('request')->input('quiz') }}">
