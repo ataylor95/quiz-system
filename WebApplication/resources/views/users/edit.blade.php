c42d82162013af3d0a7f0d474e9852adaa714db6
Alex Taylor <amt22@aber.ac.uk>
2017-03-27 17:38:11 +0100

    adding a users.edit page

---
+++ WebApplication/resources/views/users/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <form method="POST" action="{{route('users.update', ['id' => $user->id])}}">
+                    {{ csrf_field() }}
+                    {{ method_field('PATCH') }}
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" id="name" name="name" value="{{$user['name']}}" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="desc">Email:</label>
+                        <input type="text" class="form-control" id="email" name="email" value="{{$user['email']}}" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" id="session_key" name="session_key" value="{{$sessionKey}}" required>
+                    </div>
+
+                    <div class='form-group'>
+                        <button type="submit" class="btn center-block btn-default">Update</button> 
+                    </div>
+
+                    @include('layouts.form-errors')
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
