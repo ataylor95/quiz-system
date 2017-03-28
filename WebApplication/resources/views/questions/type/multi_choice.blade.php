18317422e03c1e5c8ac987e6c99d997c8ea58d33
Alex Taylor <amt22@aber.ac.uk>
2017-03-28 20:27:39 +0100

    basic multi-choice form renders

--- WebApplication/resources/views/questions/type/multi_choice.blade.php
+++ WebApplication/resources/views/questions/type/multi_choice.blade.php
@@ -1,1 +1,21 @@
-<p>Multi Choice</p>
+<div id="question-form" class="text-center">
+    <h1>{{$question->question_text}}</h1>
+    <form action="">
+        {{ csrf_field() }}
+        @for ($i=1; $i<=getQuestionsData()[0]; $i++)
+            @php $numAnswer = "answer" . $i; @endphp
+            @if (strlen($question->$numAnswer) > 0)
+            <div class="form-group">
+                <button class="btn btn-default" id="{{'answer' . $i}}" name="{{'answer' . $i}}">
+                    {{$question->$numAnswer}}
+                </button>
+            </div>
+            @endif
+        @endfor
+
+        <div class='form-group'>
+            <button type="submit" class="btn center-block btn-default" onclick='submitQuestion()'>Submit</button> 
+        </div>
+
+    </form>
+</div>
