38f51558f48b4153b1a6fbed77fb86d9ad9e15dc
Alex Taylor <amt22@aber.ac.uk>
2017-03-29 16:16:42 +0100

    refactoring some javascript code for quiz.run and adding stub for changing the page contents when websockets pushed

--- WebApplication/resources/views/quizzes/run.blade.php
+++ WebApplication/resources/views/quizzes/run.blade.php
@@ -33,24 +33,31 @@
 @section('scripts')
     <script src="https://js.pusher.com/3.1/pusher.min.js"></script>
     <script>
-      //instantiate a Pusher object with our Credential's key
-      var pusher = new Pusher('3f21a6176f0f0d31c04c', {
-          cluster: 'eu',
-          encrypted: true
-      });
+        //instantiate a Pusher object with our Credential's key
+        var pusher = new Pusher('3f21a6176f0f0d31c04c', {
+            cluster: 'eu',
+            encrypted: true
+        });
 
-      //Subscribe to the channel we specified in our Laravel Event
-      var channelName = 'quiz_' + '{{$key}}';
-      var channel = pusher.subscribe(channelName);
+        //Subscribe to the channel we specified in our Laravel Event
+        var channelName = 'quiz_' + '{{$key}}';
+        var channel = pusher.subscribe(channelName);
 
-      //Bind a function to a Event (the full Laravel class)
-      channel.bind('App\\Events\\DisplayQuiz', addMessage);
+        //Bind a function to a Event (the full Laravel class)
+        channel.bind('App\\Events\\DisplayQuiz', changeContent);
 
-      function addMessage(data) {
-        var listItem = $("<li class='list-group-item'></li>");
-        listItem.html(data.data.question_text);
-        $('#messages').prepend(listItem);
-      }
+        function addMessage(data) {
+            var listItem = $("<li class='list-group-item'></li>");
+            listItem.html(data.data.question_text);
+            $('#messages').prepend(listItem);
+        }
+
+        function changeContent(response){
+            if(response.data.type == "multi_choice"){
+                $.ajax({
+                })
+            }
+        }
     </script>
 
     @include('quizzes.admin-panel.javascript')
