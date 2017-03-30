79326b840b169298d5a781d0f52122b259ac9d4f
Alex Taylor <amt22@aber.ac.uk>
2017-03-30 14:33:09 +0100

    adding the multi select question page

--- WebApplication/resources/views/questions/type/multi_select.blade.php
+++ WebApplication/resources/views/questions/type/multi_select.blade.php
@@ -1,1 +1,18 @@
-<p>Multi Select</p>
+<div id="question-form" class="text-center">
+    <h1>{{$question->question_text}}</h1>
+        @for ($i=1; $i<=getQuestionsData()[0]; $i++)
+            @php $numAnswer = "answer" . $i; @endphp
+            @if (strlen($question->$numAnswer) > 0)
+            <div class="form-group">
+                <button class="btn btn-default" id="{{'answer' . $i}}" name="{{'answer' . $i}}" value="testy" onclick="selectMultiOption(this)">
+                    {{$question->$numAnswer}}
+                </button>
+            </div>
+            @endif
+        @endfor
+
+        <div class='form-group'>
+            <button type="submit" class="btn center-block btn-default" onclick='submitMSQuestion()'>Submit</button> 
+        </div>
+
+</div>
