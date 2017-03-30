7cd1dbf57d68af62f1698f92f68edcee25324737
Alex Taylor <amt22@aber.ac.uk>
2017-03-30 14:33:40 +0100

    javascript for submitting multi select questions

--- WebApplication/resources/views/questions/type/javascript.blade.php
+++ WebApplication/resources/views/questions/type/javascript.blade.php
@@ -14,4 +14,36 @@
             }
         });
     }
+
+    function selectMultiOption(button){
+        if ($('#' + button.id).attr('btn-success')) {
+            $('#' + button.id).toggleClass('btn-success btn-default');
+        } else {
+            $('#' + button.id).toggleClass('btn-default btn-success');
+        }
+    }
+
+    function submitMSQuestion(){
+        var answers = $('.btn-success');
+        //Use a javascript function, map,  to extract the names of all the 
+        //answers into an array
+        var result = answers.map(function(answer) {
+            return $(answers[answer]).attr('name');
+        });
+        
+        //When you call map on a jquery collection, it retuns another jquery collection
+        //This means the resultant array contains jquery collection object like prevObject
+        //
+        //This does not get accepted in the ajax request
+        //It has to be changed to an array to be used in ajax
+        result = result.toArray();
+
+        $.post({
+            url: "{{route('results')}}",
+            data: {
+                "response": result,
+                "_token": "{{csrf_token()}}"
+            }
+        });
+    }
 </script>
