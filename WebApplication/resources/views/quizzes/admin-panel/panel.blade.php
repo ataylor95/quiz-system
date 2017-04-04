95969d9e761f1f432cdf8f207ea1cfd32ed1775f
Alex Taylor <amt22@aber.ac.uk>
2017-04-04 17:14:58 +0100

    add button for showing the results on the admin end

--- WebApplication/resources/views/quizzes/admin-panel/panel.blade.php
+++ WebApplication/resources/views/quizzes/admin-panel/panel.blade.php
@@ -2,11 +2,14 @@
     {{-- TODO: right now any auth user can see this, only want the owner--}}
     <div class="admin-panel">
         <div class='row'>
-            <div class="col-xs-1">
+            <div class="col-xs-2">
                 <button id="prev-quiz" name="prev-quiz" class="btn btn-primary">Prev</button>
             </div>
-            <div class="col-xs-1">
+            <div class="col-xs-2">
                 <button id="next-quiz" name="next-quiz" class="btn btn-primary">Next</button>
+            </div>
+            <div class="col-xs-2">
+                <button id="show-results" name="show-results" class="btn btn-success">Results</button>
             </div>
             <div class="col-xs-2">
                 <button id="end-quiz" name="end-quiz" class="btn btn-danger">End Quiz</button>
