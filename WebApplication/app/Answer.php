9efbe9ae0da670906bef61e386a3c6c750ae7457
Alex Taylor <amt22@aber.ac.uk>
2017-04-04 15:40:05 +0100

    add function to get the session that an answer belongs to

--- WebApplication/app/Answer.php
+++ WebApplication/app/Answer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    protected $fillable = ['session_id', 'question', 'user_session', 'answer'];
+
+    /**
+     * Gets the session that this answer belongs to
+     *
+     * @return session
+     */
+    public function session()
+    {
+        return $this->belongsTo(Session::class);
+    }
 }
