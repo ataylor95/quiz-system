fa34701d8cf5876c415197cf1c2b1afd7282468d
Alex Taylor <amt22@aber.ac.uk>
2017-04-05 13:12:08 +0100

    created a graph object and added it to the page, with random colours and the actual data from answers

--- WebApplication/resources/views/quizzes/admin-panel/javascript.blade.php
+++ WebApplication/resources/views/quizzes/admin-panel/javascript.blade.php
@@ -1,3 +1,4 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <script>
     $('#prev-quiz').on('click', function() {
         $.ajax({
@@ -13,7 +14,14 @@
         $.ajax({
             url: "{{route('showResults', ['session_key' => $key])}}",
             success: function(data){
-                console.log(data);
+                var keys = [];
+                var values = [];
+                //create lists of the keys and values
+                jQuery.each(data, function(key, value) {
+                    keys.push(key);
+                    values.push(value);
+				});
+                createChart(keys, values);
             },
         });
     }); 
@@ -22,4 +30,69 @@
             url: "{{route('endQuiz')}}",
         });
     }); 
+
+    /**
+     * Create the chartjs object and add it to the page
+     *
+     * @param [] keys - array of keys
+     * @param [] values - array of the values
+     */
+    function createChart(keys, values){
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: keys,
+                datasets: [{
+                    label: 'Responses',
+                    data: values,
+                    borderWidth: 1,
+                    backgroundColor: getColours(keys.length)
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            userCallback: function(label, index, labels) {
+                                // when the floored value is the same as the value we have a whole number
+                                if (Math.floor(label) === label) {
+                                    return label;
+                                }
+                            },
+                        }
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+            }
+        });
+    }
+
+    /**
+     * Gets a random rgb colour for the chart
+     * Source: https://www.sitepoint.com/generating-random-color-values/
+     */
+    function getRandomColour(){
+        return 'rgb(' + (Math.floor(Math.random() * 256)) + 
+            ',' + (Math.floor(Math.random() * 256)) + 
+            ',' + (Math.floor(Math.random() * 256)) + 
+            ')';
+    }
+
+    /**
+     * Gets the array of colours for the chart
+     *
+     * @param int size - number of different results
+     */
+    function getColours(size){
+        var colourArray = [];
+        for(var i=0;i<=size;i++){
+            colourArray.push(getRandomColour());
+        }
+        return colourArray;
+    }
+
 </script>
