3fba05364842400192cfb5f779820ebe6e6355e1
Alex Taylor <amt22@aber.ac.uk>
2017-04-10 18:01:40 +0100

    add function to get the quiz that the slide belongs to

--- WebApplication/app/Slide.php
+++ WebApplication/app/Slide.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Slide extends Model
 {
-    //
+    /**
+     * Gets the quiz that this answer belongs to
+     *
+     * @return Collection quiz
+     */
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }
