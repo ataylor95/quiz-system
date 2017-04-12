96b94e36ef68a467514cd988c8f1fedd052e6388
Alex Taylor <amt22@aber.ac.uk>
2017-04-12 16:58:47 +0100

    Indication of where slides are on quiz.show pages

--- WebApplication/resources/views/quizzes/show.blade.php
+++ WebApplication/resources/views/quizzes/show.blade.php
@@ -13,49 +13,64 @@
                 <div class="panel-body">
                     <p>{{ $quiz['desc'] }}</p>
                 </div>
-                @foreach ($questions as $question)
-                    <div class='row'>
-                        <div class='col-xs-3'>
-                            <p>{{$question->question_text}}</p>
+                @foreach ($combined as $item)
+                    @if ($item->file_name)
+                        <div class="row">
+                            <div class='col-xs-5 text-center'>
+                                <p>{{$item->file_name}}</p>
+                            </div>
+                            <div class='col-xs-3'>
+                                <p>{{$item->position}}</p>
+                            </div>
+                            <div class='col-xs-3'>
+                                <a href="">View</a>
+                            </div>
                         </div>
-                        <div class='col-xs-2'>
-                            <p>{{config('questions')['types'][$question->type]}}</p>
+                    @elseif ($item->question_text)
+                        <div class='row'>
+                            <div class='col-xs-3'>
+                                <p>{{$item->question_text}}</p>
+                            </div>
+                            <div class='col-xs-2'>
+                                <p>{{config('questions')['types'][$item->type]}}</p>
+                            </div>
+                            <div class='col-xs-1'>
+                                <p>{{$item->position}}</p>
+                            </div>
+                            <div class='col-xs-1'>
+                                <form method="post" action="{{route('changePosition', ['id' => $item->id, 'direction' => 'up'])}}">
+                                    {{ csrf_field() }}
+                                    <div class='form-group'>
+                                        <button type="submit" class="glyphicon glyphicon-chevron-down"></button> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class='col-xs-1'>
+                                <form method="post" action="{{route('changePosition', ['id' => $item->id, 'direction' => 'down'])}}">
+                                    {{ csrf_field() }}
+                                    <div class='form-group'>
+                                        <button type="submit" class="glyphicon glyphicon-chevron-up"></button> 
+                                    </div>
+                                </form>
+                            </div>
+                            <div class='col-xs-1'>
+                                <a href="{{route('questions.show', ['id' => $item['id']])}}">View</a>
+                            </div>
+                            <div class='col-xs-1'>
+                                <a href="{{route('questions.edit', ['id' => $item['id'], 'quiz' => $quiz['id']])}}">Edit</a>
+                            </div>
+                            <div class='col-xs-1'>
+                                <form method="POST" action="{{route('questions.destroy', ['id' => $item['id']])}}">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <div class='form-group'>
+                                        <button type="submit" class="btn btn-default btn-danger">Delete</button> 
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <div class='col-xs-1'>
-                            <p>{{$question->position}}</p>
-                        </div>
-                        <div class='col-xs-1'>
-                            <form method="post" action="{{route('changePosition', ['id' => $question->id, 'direction' => 'up'])}}">
-                                {{ csrf_field() }}
-                                <div class='form-group'>
-                                    <button type="submit" class="glyphicon glyphicon-chevron-down"></button> 
-                                </div>
-                            </form>
-                        </div>
-                        <div class='col-xs-1'>
-                            <form method="post" action="{{route('changePosition', ['id' => $question->id, 'direction' => 'down'])}}">
-                                {{ csrf_field() }}
-                                <div class='form-group'>
-                                    <button type="submit" class="glyphicon glyphicon-chevron-up"></button> 
-                                </div>
-                            </form>
-                        </div>
-                        <div class='col-xs-1'>
-                            <a href="{{route('questions.show', ['id' => $question['id']])}}">View</a>
-                        </div>
-                        <div class='col-xs-1'>
-                            <a href="{{route('questions.edit', ['id' => $question['id'], 'quiz' => $quiz['id']])}}">Edit</a>
-                        </div>
-                        <div class='col-xs-1'>
-                            <form method="POST" action="{{route('questions.destroy', ['id' => $question['id']])}}">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <div class='form-group'>
-                                    <button type="submit" class="btn btn-default btn-danger">Delete</button> 
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                        
+                    @endif
                 @endforeach
                 <div class="panel-footer text-right">
                     <a href="{{route('questions.create', ['quiz' => $quiz['id']])}}" class="btn">Add Question</a>
